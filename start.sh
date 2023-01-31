df1de66798d4265982dd09252efb39fb956de719
akatsukishi49 <102552896+akatsukishi49@users.noreply.github.com>
2023-01-31 19:38:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/akatsukishi49/url-auto-delete-shortener-bot?organization=akatsukishi49&organization=akatsukishi49 /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
